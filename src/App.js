98f6b18516b32377aeca98e7b6520d97a1a53e91
Restaurant <elshebbaruzvidzo@gmail.com>
2025-11-11 13:41:28 +0530

    Fix submodule configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; 
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Cakes from "./pages/Cakes";
+import Events from "./pages/Events";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pages/Cakes" element={<Cakes />} />
+          <Route path="/pages/Events" element={<Events />} />
+          <Route path="/pages/About" element={<About />} />
+          <Route path="/pages/Contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
